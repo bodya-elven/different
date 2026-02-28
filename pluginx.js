7a8a737d3b892dbdf061399fd19fa13892b09559
bodya-elven <bodyanumber11@gmail.com>
2026-02-28 22:12:23 +0200

    Update pluginx.js

--- pluginx.js
+++ pluginx.js
@@ -11,8 +11,6 @@
             var html = $('<div></div>');
             var items = [];
 
-            var baseUrl = 'https://w.porno365.gold/'; // ← ЗАМІНИ НА СВІЙ ДОМЕН, якщо треба
-
             this.create = function () {
                 html.append(scroll.render());
                 this.load();
@@ -21,77 +19,86 @@
 
             this.load = function () {
                 var _this = this;
-
-                var params = {
-                    dataType: 'text',
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
-                    }
-                };
-
-                network.silent(baseUrl, function (str) {
+                var url = 'https://w.porno365.gold/'; // <--- Встав свій домен
+                
+                network.silent(url, function (htmlText) {
+                    if (!htmlText) return Lampa.Noty.show('Порожня відповідь');
+                    
                     var parser = new DOMParser();
-                    var doc = parser.parseFromString(str, 'text/html');
-
-                    // Більш гнучкі селектори (на випадок зміни верстки)
-                    var elements = doc.querySelectorAll('li.video_block, .video_block, .item, .video-item, div.video, li.video');
-
+                    var doc = parser.parseFromString(htmlText, 'text/html');
+                    var elements = doc.querySelectorAll('li.video_block');
                     var results = [];
-
+                    
                     for (var i = 0; i < elements.length; i++) {
                         var el = elements[i];
-
-                        var titleEl = el.querySelector('a.image p, p.title, .title, h3, .name, .video-title');
-                        var linkEl = el.querySelector('a.image, a[href*="movie"], a');
-                        var imgEl   = el.querySelector('img');
-
-                        if (titleEl && linkEl) {
-                            var title = (titleEl.innerText || titleEl.textContent || '').trim();
-
-                            var href = linkEl.getAttribute('href') || '';
-                            var fullUrl = href.startsWith('http') 
-                                ? href 
-                                : baseUrl.replace(/\/$/, '') + (href.startsWith('/') ? href : '/' + href);
-
-                            var img = '';
-                            if (imgEl) {
-                                img = imgEl.getAttribute('data-src') ||
-                                      imgEl.getAttribute('data-lazy') ||
-                                      imgEl.getAttribute('data-original') ||
-                                      imgEl.getAttribute('src') || '';
-                            }
-
-                            if (img && !img.startsWith('http')) {
-                                img = baseUrl.replace(/\/$/, '') + (img.startsWith('/') ? img : '/' + img);
-                            }
-
+                        var linkEl = el.querySelector('a.image');
+                        var titleEl = el.querySelector('a.image p');
+                        var imgEl = el.querySelector('div.tumba img'); // Беремо постер одразу
+                        
+                        if (linkEl && titleEl) {
+                            var imgSrc = imgEl ? imgEl.getAttribute('src') : '';
+                            if (imgSrc && imgSrc.indexOf('//') === 0) imgSrc = 'https:' + imgSrc;
+                            
                             results.push({
-                                title: title,
-                                url: fullUrl,
-                                poster: img || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
-                                img: img || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+                                title: titleEl.innerText.trim(),
+                                url: linkEl.getAttribute('href'),
+                                img: imgSrc // Передаємо картинку в стандартну картку
                             });
                         }
                     }
-
+                    
                     if (results.length > 0) {
                         _this.build(results);
                     } else {
-                        html.append('<div style="color:#ff4444; padding: 30px; text-align:center;">Нічого не знайдено.<br>Можливо, змінилася структура сайту.</div>');
+                        Lampa.Noty.show('Не знайдено відео');
                     }
-                }, function () {
-                    html.append('<div style="color:#ff4444; padding: 30px; text-align:center;">Помилка завантаження сайту.<br>Перевірте інтернет або домен.</div>');
-                }, false, params);
+                }, function() { Lampa.Noty.show('Помилка мережі'); }, false, { dataType: 'text' });
             };
 
             this.build = function (data) {
-                items = [];
-
                 for (var i = 0; i < data.length; i++) {
-                    var card = new Lampa.Card(data[i], { card_category: false });
-                    card.create();
-                    scroll.append(card.render());
-                    items.push(card);
+                    (function(element) {
+                        var card = new Lampa.Card(element, { card_category: false });
+                        card.create();
+                        
+                        card.render().on('hover:enter', function () {
+                            Lampa.Activity.loader(true);
+                            network.silent(element.url, function(vHtml) {
+                                Lampa.Activity.loader(false);
+                                if (!vHtml) return Lampa.Noty.show('Помилка завантаження');
+                                
+                                var p = new DOMParser();
+                                var d = p.parseFromString(vHtml, 'text/html');
+                                var videoStreams = [];
+                                
+                                var qLinks = d.querySelectorAll('.quality_chooser a');
+                                for (var j = 0; j < qLinks.length; j++) {
+                                    videoStreams.push({ title: qLinks[j].innerText.trim(), url: qLinks[j].getAttribute('href') });
+                                }
+                                
+                                if (videoStreams.length === 0) {
+                                    var mainPlayBtn = d.querySelector('a.btn-play.play-video');
+                                    if (mainPlayBtn) {
+                                        var mainUrl = mainPlayBtn.getAttribute('href');
+                                        if (mainUrl) videoStreams.push({ title: 'Оригінал', url: mainUrl });
+                                    }
+                                }
+                                
+                                if (videoStreams.length > 0) {
+                                    var bestStream = videoStreams[videoStreams.length - 1];
+                                    Lampa.Player.play({ title: element.title, url: bestStream.url, quality: videoStreams });
+                                    Lampa.Player.playlist([{ title: element.title, url: bestStream.url, quality: videoStreams }]);
+                                } else {
+                                    Lampa.Noty.show('Не знайдено відео');
+                                }
+                            }, function() { 
+                                Lampa.Activity.loader(false); 
+                            }, false, { dataType: 'text' });
+                        });
+
+                        scroll.append(card.render());
+                        items.push(card);
+                    })(data[i]);
                 }
 
                 Lampa.Controller.add('content', {
@@ -99,12 +106,11 @@
                         Lampa.Controller.collectionSet(scroll.render());
                         Lampa.Controller.collectionFocus(items.length ? items[0].render()[0] : false, scroll.render());
                     },
+                    up: function () { Lampa.Controller.collectionFocus(false, scroll.render()).up; },
+                    down: function () { Lampa.Controller.collectionFocus(false, scroll.render()).down; },
                     left: function () { Lampa.Controller.toggle('menu'); },
-                    right: function () { Lampa.Controller.collectionFocus(false, scroll.render()).right(); },
-                    up: function () { Lampa.Controller.collectionFocus(false, scroll.render()).up(); },
-                    down: function () { Lampa.Controller.collectionFocus(false, scroll.render()).down(); }
+                    right: function () { Lampa.Controller.collectionFocus(false, scroll.render()).right; }
                 });
-
                 Lampa.Controller.toggle('content');
             };
 
@@ -120,11 +126,9 @@
         function addMenu() {
             var menuList = $('.menu .menu__list').eq(0);
             if (!menuList.length) return;
-
             if (menuList.find('[data-action="my_custom_catalog"]').length === 0) {
-                var myMenu = '<li class="menu__item selector" data-action="my_custom_catalog"><div class="menu__text">Мій Каталог (Porno365)</div></li>';
+                var myMenu = '<li class="menu__item selector" data-action="my_custom_catalog"><div class="menu__text">Мій Каталог</div></li>';
                 menuList.append(myMenu);
-
                 $('.menu__item[data-action="my_custom_catalog"]').on('hover:enter', function () {
                     Lampa.Activity.push({
                         title: 'Мій Каталог',
