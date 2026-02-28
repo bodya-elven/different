1faf70c49b1b62fe3a1e7ab7a7c597143f6175a9
bodya-elven <bodyanumber11@gmail.com>
2026-02-28 19:28:51 +0200

    Update pluginx.js

--- pluginx.js
+++ pluginx.js
@@ -1,48 +1,6 @@
 (function () {
     'use strict';
 
-    // CSS для мобільного дизайну (одна колонка, повні назви)
-    var customCss = `
-    <style>
-        /* Примусово робимо сітку в одну колонку на мобільних */
-        @media (max-width: 768px) {
-            .custom_catalog_page .scroll__body {
-                display: flex !important;
-                flex-direction: column !important;
-                padding: 10px !important;
-            }
-            .custom_catalog_page .card {
-                width: 100% !important;
-                margin-bottom: 20px !important;
-            }
-        }
-        
-        /* Робимо постер 16:9 (широким) */
-        .custom_catalog_page .card__view {
-            padding-bottom: 56.25% !important; 
-        }
-        .custom_catalog_page .card__img {
-            object-fit: cover !important;
-        }
-        
-        /* Повні назви у кілька рядків */
-        .custom_catalog_page .card__title {
-            white-space: normal !important;
-            height: auto !important;
-            line-height: 1.3 !important;
-            padding-top: 8px !important;
-            text-align: left !important;
-        }
-        
-        /* Ховаємо зайве */
-        .custom_catalog_page .card__age, 
-        .custom_catalog_page .card__textbox {
-            display: none !important;
-        }
-    </style>
-    `;
-    $('body').append(customCss);
-
     // 1. Кнопка в меню
     Lampa.Listener.follow('app', function (e) {
         if (e.type == 'ready') {
@@ -57,7 +15,7 @@
 
             $('.menu__item[data-action="my_custom_catalog"]').on('hover:enter', function () {
                 Lampa.Activity.push({
-                    url: 'https://w.porno365.gold/', // <--- Встав свій домен
+                    url: 'https://w.porno365.gold/', // <--- ЗАЛИШ ОДИНАРНІ ЛАПКИ
                     title: 'Мій Каталог',
                     component: 'custom_catalog_comp',
                     page: 1
@@ -66,28 +24,27 @@
         }
     });
 
-    // 2. Компонент каталогу (ВИКОРИСТОВУЄМО InteractionCategory ЯК У ПРИКЛАДІ)
+    // 2. Компонент каталогу (Нативний Lampa)
     function CustomCatalog(object) {
-        // Успадковуємо всю магію скролінгу від Lampa
         var comp = new Lampa.InteractionCategory(object);
         var network = new Lampa.Reguest();
 
         comp.create = function () {
             var _this = this;
             
-            // Додаємо клас, щоб наші CSS-стилі застосувалися тільки тут
-            this.html.addClass('custom_catalog_page');
-
             network.silent(object.url, function (responseText) {
                 var parser = new DOMParser();
                 var doc = parser.parseFromString(responseText, 'text/html');
                 var results = [];
 
                 var elements = doc.querySelectorAll('li.video_block'); 
-
-                elements.forEach(function(el) {
+                
+                // БЕЗПЕЧНИЙ ЦИКЛ FOR (щоб уникнути Script error на старих ТВ)
+                for (var i = 0; i < elements.length; i++) {
+                    var el = elements[i];
                     var linkEl = el.querySelector('a.image'); 
-                    var imgEl = el.querySelector('div.tumba img');   
+                    // Беремо будь-яку картинку всередині блоку (згідно з твоїм фото fa2fe6c8)
+                    var imgEl = el.querySelector('img'); 
                     var titleEl = el.querySelector('a.image p');    
 
                     if (linkEl && imgEl && titleEl) {
@@ -102,10 +59,11 @@
                             url: linkEl.getAttribute('href')
                         });
                     }
-                });
+                }
 
                 if (results.length > 0) {
-                    // Передаємо дані у вбудований будівник Lampa
+                    // НАЙГОЛОВНІШЕ: Вмикаємо нативний горизонтальний дизайн "wide"
+                    Lampa.Utils.extendItemsParams(results, { style: { name: 'wide' } });
                     _this.build({ results: results });
                 } else {
                     _this.empty();
@@ -113,60 +71,64 @@
             }, this.empty.bind(this), false, { dataType: 'text' });
         };
 
-        // 3. Перехоплюємо клік по картці, щоб запустити плеєр, а не відкривати сторінку фільму
+        // 3. Побудова карток і перехоплення кліку
         comp.append = function (data) {
             var _this = this;
-            data.results.forEach(function (element) {
-                // Вказуємо card_category: false, щоб це не виглядало як папка
+            
+            for (var k = 0; k < data.results.length; k++) {
+                var element = data.results[k];
                 var card = new Lampa.Card(element, { card_category: false });
                 card.create();
                 
-                // Перевизначаємо дію при натисканні
-                card.onHover('enter', function () {
-                    network.silent(element.url, function(videoPageHtml) {
-                        var parser = new DOMParser();
-                        var doc = parser.parseFromString(videoPageHtml, 'text/html');
-                        var videoStreams = []; 
+                // ВИПРАВЛЕНО: card.render().on замість card.onHover
+                card.render().on('hover:enter', function () {
+                    // Замикання для збереження контексту element
+                    (function(currentElement) {
+                        network.silent(currentElement.url, function(videoPageHtml) {
+                            var parser = new DOMParser();
+                            var doc = parser.parseFromString(videoPageHtml, 'text/html');
+                            var videoStreams = []; 
 
-                        var qualityLinks = doc.querySelectorAll('.quality_chooser a');
-                        qualityLinks.forEach(function(link) {
-                            var videoUrl = link.getAttribute('href');
-                            var qualityName = link.innerText.trim() || link.getAttribute('data-quality');
-                            if (videoUrl) {
-                                videoStreams.push({ title: qualityName || 'Відео', url: videoUrl });
-                            }
-                        });
-
-                        if (videoStreams.length === 0) {
-                            var mainPlayBtn = doc.querySelector('a.btn-play.play-video');
-                            if (mainPlayBtn) {
-                                var mainUrl = mainPlayBtn.getAttribute('href');
-                                if (mainUrl) videoStreams.push({ title: 'Оригінал', url: mainUrl });
-                            }
-                        }
-
-                        if (videoStreams.length > 0) {
-                            var bestStream = videoStreams[videoStreams.length - 1];
-
-                            var playlist = [{
-                                title: element.title,
-                                url: bestStream.url, 
-                                quality: videoStreams 
-                            }];
+                            var qualityLinks = doc.querySelectorAll('.quality_chooser a');
                             
-                            Lampa.Player.play(playlist[0]);
-                            Lampa.Player.playlist(playlist);
-                        } else {
-                            Lampa.Noty.show('Не знайдено посилання на плеєр');
-                        }
-                    }, false, false, { dataType: 'text' });
+                            for (var j = 0; j < qualityLinks.length; j++) {
+                                var link = qualityLinks[j];
+                                var videoUrl = link.getAttribute('href');
+                                var qualityName = link.innerText.trim() || link.getAttribute('data-quality');
+                                if (videoUrl) {
+                                    videoStreams.push({ title: qualityName || 'Відео', url: videoUrl });
+                                }
+                            }
+
+                            if (videoStreams.length === 0) {
+                                var mainPlayBtn = doc.querySelector('a.btn-play.play-video');
+                                if (mainPlayBtn) {
+                                    var mainUrl = mainPlayBtn.getAttribute('href');
+                                    if (mainUrl) videoStreams.push({ title: 'Оригінал', url: mainUrl });
+                                }
+                            }
+
+                            if (videoStreams.length > 0) {
+                                var bestStream = videoStreams[videoStreams.length - 1];
+                                var playlist = [{
+                                    title: currentElement.title,
+                                    url: bestStream.url, 
+                                    quality: videoStreams 
+                                }];
+                                
+                                Lampa.Player.play(playlist[0]);
+                                Lampa.Player.playlist(playlist);
+                            } else {
+                                Lampa.Noty.show('Не знайдено посилання на плеєр');
+                            }
+                        }, false, false, { dataType: 'text' });
+                    })(element);
                 });
 
                 _this.appendBody(card);
-            });
+            }
         };
 
-        // Вимикаємо пагінацію, оскільки ми парсимо лише одну сторінку HTML
         comp.nextPageReuest = function () {};
 
         return comp;
@@ -174,79 +136,7 @@
 
     Lampa.Component.add('custom_catalog_comp', CustomCatalog);
 })();
-            overflow: hidden !important;
-        }
-        
-        /* Зображення заповнює весь блок постеру */
-        .custom-catalog-list .card__img {
-            object-fit: cover !important;
-            width: 100% !important;
-            height: 100% !important;
-            position: absolute !important;
-            top: 0 !important;
-            left: 0 !important;
-        }
-        
-        /* Гарний багаторядковий текст під постером (повна назва) */
-        .custom-catalog-list .card__title {
-            white-space: normal !important;
-            text-align: left !important;
-            padding-top: 10px !important;
-            line-height: 1.4 !important;
-            font-size: 1.1em !important;
-            height: auto !important;
-            display: block !important;
-        }
-        
-        .custom-catalog-list .card__age, .custom-catalog-list .card__textbox {
-            display: none !important;
-        }
-    </style>
-    `;
-    $('body').append(customCss);
-
-    Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') {
-            var myMenu = '<li class="menu__item selector" data-action="my_custom_catalog">' +
-                         '<div class="menu__ico">' +
-                         '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>' +
-                         '</div>' +
-                         '<div class="menu__text">Мій Каталог</div>' +
-                         '</li>';
-
-            $('.menu .menu__list').eq(0).append(myMenu);
-
-            $('.menu__item[data-action="my_custom_catalog"]').on('hover:enter', function () {
-                Lampa.Activity.push({
-                    url: 'https://w.porno365.gold/', // <--- Залиш одинарні лапки!
-                    title: 'Мій Каталог',
-                    component: 'custom_catalog_comp',
-                    page: 1
-                });
-            });
-        }
-    });
-
-    function CustomCatalog(object) {
-        var network = new Lampa.Reguest(); 
-        var scroll  = new Lampa.Scroll({ mask: true, over: true }); // Повернули Lampa.Scroll
-        var html    = $('<div></div>');
-        var body    = $('<div class="custom-catalog-list"></div>');
-        var items   = [];
-
-        this.create = function () {
-            // Додаємо клас для активації нашого мобільного CSS-скролу
-            html.addClass('custom-catalog-scroll');
-            html.append(scroll.render());
-            scroll.append(body);
-            this.load();
-            return this.render();
-        };
-
-        this.load = function () {
-            var url = object.url; 
-            network.silent(url, this.parseHTML.bind(this), function () {
-                Lampa.Noty.show('Помилка завантаження сайту');
+');
             }, false, { dataType: 'text' });
         };
 
